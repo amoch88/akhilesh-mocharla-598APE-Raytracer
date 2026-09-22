158c3aacc1f6bfa60f55b6e82fd051803e37b063
am88 <am88@fa26-cs598a-022.cs.illinois.edu>
2026-09-21 21:59:31 -0500

    Parallelize pixel rendering with OpenMP

--- main.cpp
+++ main.cpp
@@ -46,9 +46,13 @@ void set(int i, int j, unsigned char r, unsigned char g, unsigned char b){
 }
 
 void refresh(Autonoma* c){
+   #pragma omp parallel for schedule(static)
    for(int n = 0; n<H*W; ++n) 
    { 
-      Vector ra = c->camera.forward+((double)(n%W)/W-.5)*((c->camera.right))+(.5-(double)(n/W)/H)*((c->camera.up));
+      Vector ra = c->camera.forward
+         + ((double)(n%W)/W-.5)*(c->camera.right)
+         + (.5-(double)(n/W)/H)*(c->camera.up);
+
       calcColor(&DATA[3*n], c, Ray(c->camera.focus, ra), 0);
    }
 }
