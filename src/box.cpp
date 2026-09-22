4de549bfa09a9720ac6ddd82b3626a9954c4909d
Akhilesh Mocharla <am88@illinois.edu>
2026-09-21 22:25:22 -0500

    Reduce coordinate work in box intersections

--- src/box.cpp
+++ src/box.cpp
@@ -4,11 +4,19 @@ Box::Box(const Vector &c, Texture* t, double ya, double pi, double ro, double tx
 Box::Box(const Vector &c, Texture* t, double ya, double pi, double ro, double tx):Plane(c, t, ya, pi, ro, tx,tx){}
 
 double Box::getIntersection(const Ray& ray){
-   double time = Plane::getIntersection(ray);
-   Vector dist = solveScalers(right, up, vect, ray.point+ray.vector*time-center);
-   if(time==inf) 
-      return time;
-   return ( ((dist.x>=0)?dist.x:-dist.x)>textureX/2 || ((dist.y>=0)?dist.y:-dist.y)>textureY/2 )?inf:time;
+   const double time = Plane::getIntersection(ray);
+
+   if(time == inf)
+      return inf;
+
+   const Vector offset = ray.point + ray.vector * time - center;
+
+   const double x = offset.dot(right);
+   const double y = offset.dot(up);
+
+   return (((x >= 0) ? x : -x) > textureX / 2 ||
+           ((y >= 0) ? y : -y) > textureY / 2)
+          ? inf : time;
 }
 
 bool Box::getLightIntersection(const Ray& ray, double* fill){
@@ -16,13 +24,18 @@ bool Box::getLightIntersection(const Ray& ray, double* fill){
    const double norm = vect.dot(ray.point)+d;
    const double r = -norm/t;
    if(r<=0. || r>=1.) return false;
-   Vector dist = solveScalers(right, up, vect, ray.point+ray.vector*r-center);
-   if( ((dist.x>=0)?dist.x:-dist.x)>textureX/2 || ((dist.y>=0)?dist.y:-dist.y)>textureY/2 ) return false;
+
+   const Vector offset = ray.point + ray.vector * r - center;
+
+   const double distX = offset.dot(right);
+   const double distY = offset.dot(up);
+
+   if( ((distX>=0)?distX:-distX)>textureX/2 || ((distY>=0)?distY:-distY)>textureY/2 ) return false;
 
    if(texture->opacity>1-1E-6) return true;   
    unsigned char temp[4];
    double amb, op, ref;
-   texture->getColor(temp, &amb, &op, &ref,fix(dist.x/textureX-.5), fix(dist.y/textureY-.5));
+   texture->getColor(temp, &amb, &op, &ref,fix(distX/textureX-.5), fix(distY/textureY-.5));
    if(op>1-1E-6) return true;
    fill[0]*=temp[0]/255.;
    fill[1]*=temp[1]/255.;
